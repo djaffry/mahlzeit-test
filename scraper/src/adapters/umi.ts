3e5b54b1991df972f33cd64f354f927bd536145f
djaffry <5738259+djaffry@users.noreply.github.com>
2026-02-28 21:02:50 +0100

    feat: add umi

---
+++ scraper/src/adapters/umi.ts
@@ -0,0 +1,10 @@
+import type { LinkAdapter } from '../types.js';
+
+const adapter: LinkAdapter = {
+  id: 'umi',
+  title: '🍣 Umi',
+  url: 'https://umi-12.jimdosite.com/umi-nordbahn-1020/',
+  type: 'link',
+};
+
+export default adapter;
